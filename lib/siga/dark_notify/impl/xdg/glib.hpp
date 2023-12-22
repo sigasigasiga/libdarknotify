3984b2654e6cab78ded30961c9466183a983499f
Yegor Bychin <j@siga.icu>
2023-12-22 22:10:13 +0000

    initial linux support implementation

---
+++ lib/siga/dark_notify/impl/xdg/glib.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "siga/dark_notify/dark_notify.hpp"
+
+#include <gio/gio.h>
+#include <glib-object.h>
+
+namespace siga::dark_notify::impl::xdg {
+
+class glib_t : public dark_notify_t
+{
+public:
+    glib_t();
+
+private: // dark_notify_t
+    void register_callback(callback_t callback) final;
+    void unregister_callback() final;
+
+    appearance_t query() final;
+
+    void tick() final;
+    void run() final;
+    void stop() final;
+
+private:
+    class g_unref_t
+    {
+    public:
+        void operator()(gpointer ptr) { ::g_object_unref(ptr); }
+    };
+
+    template<typename T>
+    using ptr_t = std::unique_ptr<T, g_unref_t>;
+
+private:
+    ptr_t<GMainLoop> loop_;
+    ptr_t<GDBusProxy> portal_;
+
+    callback_t cb_;
+    gulong signal_id_;
+};
+
+} // namespace siga::dark_notify::impl::xdg
